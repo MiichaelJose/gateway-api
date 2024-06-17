df924c1dcd5fa502effa0c66d85dda2cc843c7ef
MiichaelJose <michaeljosecraft1@gmail.com>
2024-06-16 23:07:20 -0300

    add .env

--- src/server.ts
+++ src/server.ts
@@ -4,53 +4,56 @@ import { createProxyMiddleware } from 'http-proxy-middleware';
 
 import { expressjwt } from "express-jwt";
 
+import 'dotenv/config';
+
 const router = express();
 
-const SECRET_KEY = Buffer.from("node-auth").toString('base64');;
-const PORT = 3000;
+const ENV: any = process.env
 
 const CHECK_JWT = expressjwt({
-    secret: SECRET_KEY,
+    secret: Buffer.from(ENV.SECRET_KEY).toString('base64'),
     algorithms: ['HS256']
 });
 
-const CONF_MIDDLEWARE= {
-  target: 'http://localhost:3002/', 
-  changeOrigin: true,
-  on: {
-    proxyReq: (proxyReq: any, req: any, res: any) => {
-      /* handle proxyReq */
-      proxyReq.setHeader('X-Proxy-Header', 'secret-value');
-    },
-    proxyRes: (proxyRes: any, req: any, res: any) => {
-      /* handle proxyRes */
-    },
-    error: (err: any, req: any, res: any) => {
-      res.status(500).send('Proxy Error');
-    },
+const CONF_MIDDLEWARE = (host: any) => {
+  return {
+    target: host, 
+    changeOrigin: true,
+    on: {
+      proxyReq: (proxyReq: any, req: any, res: any) => {
+        /* handle proxyReq */
+        proxyReq.setHeader('X-Proxy-Header', 'secret-value');
+      },
+      proxyRes: (proxyRes: any, req: any, res: any) => {
+        /* handle proxyRes */
+      },
+      error: (err: any, req: any, res: any) => {
+        console.log(res);
+        
+        res.status(500).send('Proxy Error');
+      },
+    }
   }
 }
 
 const VALIDATION_REQUEST = (err: any, req: any, res: any, next: any) => {
   const proxyHeader = req.headers['x-proxy-header'];
-
   if (err.name === "UnauthorizedError") {
     res.status(401).send("invalid token...");
   } else {
     if(proxyHeader !== 'secret-value') {
       res.status(403).send('Acesso negado.');
     }
-
     next(err);
   }
-
 }
-router.use('/api/service-task', CHECK_JWT, createProxyMiddleware(CONF_MIDDLEWARE))
 
-router.use('/api/service-auth', createProxyMiddleware({ target: 'http://localhost:3001/singin', changeOrigin: true }));
+router.use('/api/service-task', CHECK_JWT, createProxyMiddleware(CONF_MIDDLEWARE(ENV.HOST_TASK)))
+
+router.use('/api/service-auth', createProxyMiddleware(CONF_MIDDLEWARE(ENV.HOST_AUTH)));
 
 router.use(VALIDATION_REQUEST);
 
-router.listen(PORT, () => {
-    console.log(`Server is running on port : ${PORT} `);
+router.listen(ENV.PORT, () => {
+    console.log(`Server is running on port : ${ENV.PORT} `);
 })
